fd0aa584e3065acc9cb1b92b8b7273dab47e3da9
Nitewa1kr <adeel.khilji83@gmail.com>
2017-02-09 03:06:05 -0500

    added UPDATE and DELETE functions in script.js and added the app.put and app.delete in the server.js These studies were to understand how to work with Express.JS, NODE.JS, JQuery, JSON, AJAX

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,5 @@
 $(function(){
+
     //GET/READ
     $('#get-button').on('click', function(){
         $.ajax({
@@ -6,8 +7,83 @@ $(function(){
            contentType: 'application/json',
            success: function(response)
            {
-               console.log(response);
+               var tbodyEL = $('tbody');
+
+               tbodyEL.html('');
+
+               response.products.forEach(function(product)
+               {
+                    tbodyEL.append('\
+                    <tr>\
+                        <td class="id">'+product.id+'</td>\
+                        <td><input type="text" class="name" value="'+product.name+'"></td>\
+                        <td>\
+                            <button class="update-button">UPDATE/PUT</button>\
+                            <button class="delete-button">DELETE</button>\
+                        </td>\
+                    </tr>\
+                    ');
+               });
            } 
         });
     });
+
+    // CREATE/POST
+    $('#create-form').on('submit',function(event){
+        event.preventDefault();
+
+        var createinput = $('#create-input');
+
+        $.ajax({
+            url: '/products',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({name: createinput.val()}),
+            success: function(response){
+                console.log(response);
+                createinput.val('');
+                $('#get-button').click();
+            }
+        });
+    });
+
+    //UPDATE/PUT
+    $('table').on('click', '.update-button',function()
+    {
+        var rowEL = $(this).closest('tr');
+        var id = rowEL.find('.id').text();
+        var newName = rowEL.find('.name').val();
+
+        $.ajax({
+            url: '/products/' + id,
+            method: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({newName: newName}),
+            success: function(response)
+            {
+                console.log(response)
+                $('#get-button').click();
+            }
+        });
+    });
+
+    //DELETE
+    $('table').on('click', '.delete-button', function()
+    {
+        var rowEL = $(this).closest('tr');
+        var id = rowEL.find('.id').text();
+
+        $.ajax({
+            url: '/products/' + id,
+            method: 'DELETE',
+            contentType: 'application/json',
+            success: function(response)
+            {
+                console.log(response)
+                $('#get-button').click();
+            }
+        });
+    });
 });
+
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 //JOSN OBJECT
 var products = 
@@ -19,12 +20,59 @@ var currentID = 2;
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+
 //GET PRODUCTS.
 app.get('/products', function(req, res)
 {
-    res.send('SUCCESS!');
+    res.send({products: products});
 });
 
+//CREATE PRODUCTS
+app.post('/products', function(req,res)
+{
+    var productName = req.body.name;
+    currentID++;
+
+    products.push({
+        id: currentID,
+        name: productName
+    });
+    res.send('Successfully created product!');
+});
+
+//UPDATE PRODUCTS
+app.put('/products/:id', function(req, res){
+    var id = req.params.id;
+    var newName = req.body.newName;
+    var found = false;
+    products.forEach(function(product, index)
+    {
+        if(!found && product.id === Number(id))
+        {
+            product.name = newName;
+        }
+    });
+    res.send('Successfully updated product!');
+});
+
+//DELETE
+app.delete('/products/:id', function(req, res){
+    var id = req.params.id;
+    var found = false;
+
+    products.forEach(function(product, index)
+    {
+        if(!found && product.id === Number(id))
+        {
+            products.splice(index, 1);
+        }
+    });
+    res.send('Product successfully deleted');
+});
+
+//LISTEN ON
 app.listen(PORT, function(){
-    console.log('Server Listening on' + PORT);
+    console.log('Server Listening on ' + PORT);
 });
